654ce3230863ded10bfbb1210830e2c5cdcf120d
Emil Martini <emil.martini@hotmail.com>
2020-02-26 19:06:54 +0100

    fix addbook method

--- script.js
+++ script.js
@@ -7,17 +7,30 @@ function fetchAPI(){
   fetch(APIUrl)
     .then((response) =>
       response.json())
-    .then((key) => {main.innerHTML = key.key; myKey = key.key; console.log(key.key)})
+    .then((jsonResponse) => {main.innerHTML = jsonResponse.key; myKey = jsonResponse.key})
   };
 
-function fetchBooks(){
-  
+function addBook(){
   //Något som inte funkar xddddd, men på rätt väg i guess
   var url = 'https://www.forverkliga.se/JavaScript/api/crud.php?key=';  //hittade urlen i dokumentationen, tror den är fel
-  var fetchRequest = url + myKey + '&op=insert' + document.getElementById("title").value + document.getElementById("author").value; //lägger till urlen, och alla värdena i en sträng
-  var main = document.getElementById("test"); //tar en paragraf ur htmlen
+  var fetchRequest = url + myKey + '&op=insert&title=' + document.getElementById("title").value + "&author=" + document.getElementById("author").value; //lägger till urlen, och alla värdena i en sträng
+  var main = document.getElementById("test"); //tar ett element ur htmlen
   main.innerHTML =  "Adding book..."; //skriver bara 'adding book'
-  fetch(fetchRequest).then((response) => response.json()).then((key) => {main.innerHTML = key.message; console.log(key)}); //magic, loggar till konsollen och skriver ut. men får bara en invalid operation error typ
+  fetchFromApi(fetchRequest, main); //kallar på en metod för att kunna kör den rekursivt, passerar in requesten och html referensen för att kunna använda samma metod för flera saker
+}
+
+function fetchFromApi(fetchRequest, htmlElement){
+  fetch(fetchRequest) //fetchar requesten
+  .then((response) => 
+    response.json() //gör om till json
+  ).then((jsonResponse) => 
+  {
+    console.log(jsonResponse.status);  //loggar statusen i debug syfte
+    if(jsonResponse.status == "error"){  //om statusen är 'error'
+      fetchFromApi(fetchRequest, htmlElement);  //kör samma metod igen
+    } else {
+    htmlElement.innerHTML = jsonResponse.id; //skriv ut id på 'transaktionen'
+  }});
 }
 
 document.getElementById("requestAPIKey").onclick = function(){
@@ -25,5 +38,5 @@ document.getElementById("requestAPIKey").onclick = function(){
 }
 
 document.getElementById("addBookBtn").onclick = function(){
-  fetchBooks();
+  addBook();
 };
