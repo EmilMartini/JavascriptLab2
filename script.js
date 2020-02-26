7f0e6acfc81ec6a8feb6b67670ef13c981e98024
Emil Martini <emil.martini@hotmail.com>
2020-02-26 16:14:28 +0100

    begin adding books post method

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ var myKey;
 
 function fetchAPI(){
   var APIUrl = "https://www.forverkliga.se/JavaScript/api/crud.php?requestKey";
-
-  const main = document.getElementById("result");
+  var main = document.getElementById("result");
   main.innerHTML = "Loading...";
   fetch(APIUrl)
     .then((response) =>
@@ -11,22 +10,20 @@ function fetchAPI(){
     .then((key) => {main.innerHTML = key.key; myKey = key.key; console.log(key.key)})
   };
 
+function fetchBooks(){
+  
+  //Något som inte funkar xddddd, men på rätt väg i guess
+  var url = 'https://www.forverkliga.se/JavaScript/api/crud.php?key=';  //hittade urlen i dokumentationen, tror den är fel
+  var fetchRequest = url + myKey + '&op=insert' + document.getElementById("title").value + document.getElementById("author").value; //lägger till urlen, och alla värdena i en sträng
+  var main = document.getElementById("test"); //tar en paragraf ur htmlen
+  main.innerHTML =  "Adding book..."; //skriver bara 'adding book'
+  fetch(fetchRequest).then((response) => response.json()).then((key) => {main.innerHTML = key.message; console.log(key)}); //magic, loggar till konsollen och skriver ut. men får bara en invalid operation error typ
+}
+
 document.getElementById("requestAPIKey").onclick = function(){
   fetchAPI();
 }
 
-document.getElementById("addBookForm").onsubmit = function(){
-  var newObject = {
-    op : "insert",
-    key : myKey,
-    title : document.getElementById("title").value,
-    author : document.getElementById("author").value
-  };
-  queryString = Object.keys(newObject).map(key => key + '=' + newObject[key]).join('&');
-  var currentUrl = (window.location.href).split('?');
-  currentUrl[1] = queryString;
-  window.location.search = queryString;
-  this.reset();
+document.getElementById("addBookBtn").onclick = function(){
+  fetchBooks();
 };
-
-fetchAPI();
