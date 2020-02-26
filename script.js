e408819431624fcc213bee8696747c13c2603458
Emil Martini <emil.martini@hotmail.com>
2020-02-26 13:23:44 +0100

    fix get api method

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-var yourKey;
+var myKey;
 
+function fetchAPI(){
+  var APIUrl = "https://www.forverkliga.se/JavaScript/api/crud.php?requestKey";
 
-function httpGet()
-{
-    var APIUrl = "https://www.forverkliga.se/JavaScript/api/crud.php?requestKey";
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", APIUrl, false );
-    xmlHttp.send();
-    return xmlHttp.responseText;
-}
+  const main = document.getElementById("result");
+  main.innerHTML = "Loading...";
+  fetch(APIUrl)
+    .then((response) => response.json())
+    .then((key) => {main.innerHTML = key.key; myKey = key.key; console.log(key.key)})
+  };
 
 document.getElementById("requestAPIKey").onclick = function(){
-  var result = JSON.parse(httpGet());   //status, key
-  yourKey = result.key;
-  document.getElementById("result").innerHTML = yourKey;
+  fetchAPI();
+}
+
+document.getElementById("addBookForm").onsubmit = function(){
+  var newObject = {
+    op : "insert",
+    key : myKey,
+    title : document.getElementById("title").value,
+    author : document.getElementById("author").value
+  };
+  queryString = Object.keys(newObject).map(key => key + '=' + newObject[key]).join('&');
+  var currentUrl = (window.location.href).split('?');
+  currentUrl[1] = queryString;
+  window.location.search = queryString;
 };
+
+fetchAPI();
