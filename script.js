b3bea0e64c83ea01271a818395fd10da150a83e3
Emil Martini <emil.martini@hotmail.com>
2020-02-26 11:08:39 +0100

    add get method for api key

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-alert("Hej");
+var yourKey;
+
+
+function httpGet()
+{
+    var APIUrl = "https://www.forverkliga.se/JavaScript/api/crud.php?requestKey";
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", APIUrl, false );
+    xmlHttp.send();
+    return xmlHttp.responseText;
+}
+
+document.getElementById("requestAPIKey").onclick = function(){
+  var result = JSON.parse(httpGet());   //status, key
+  yourKey = result.key;
+  document.getElementById("result").innerHTML = yourKey;
+};
