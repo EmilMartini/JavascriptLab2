2dc4cb8838ee67034df2cadee80aa7809f9bf685
Emil Martini <emil.martini@hotmail.com>
2020-02-26 14:02:38 +0100

    change value of button

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ function fetchAPI(){
   const main = document.getElementById("result");
   main.innerHTML = "Loading...";
   fetch(APIUrl)
-    .then((response) => response.json())
+    .then((response) =>
+      response.json())
     .then((key) => {main.innerHTML = key.key; myKey = key.key; console.log(key.key)})
   };
 
@@ -25,6 +26,7 @@ document.getElementById("addBookForm").onsubmit = function(){
   var currentUrl = (window.location.href).split('?');
   currentUrl[1] = queryString;
   window.location.search = queryString;
+  this.reset();
 };
 
 fetchAPI();
