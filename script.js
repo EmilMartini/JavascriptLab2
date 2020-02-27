ed0ba351182f78634e7c7953557cc9fcb82b1572
Emil Martini <emil.martini@hotmail.com>
2020-02-27 09:32:36 +0100

    test bootstrap

--- script.js
+++ script.js
@@ -14,12 +14,13 @@ function addBook(){
   var counter = 1;  //counter för att se till att vi max checkar 10 gånger
   var url = 'https://www.forverkliga.se/JavaScript/api/crud.php?key=';  //hittade urlen i dokumentationen, tror den är fel
   var fetchRequest = url + myKey + '&op=insert&title=' + document.getElementById("title").value + "&author=" + document.getElementById("author").value; //lägger till urlen, och alla värdena i en sträng
-  var main = document.getElementById("test"); //tar ett element ur htmlen
-  main.innerHTML =  "Adding book..."; //skriver bara 'adding book'
-  fetchFromApi(fetchRequest, main, counter); //kallar på en metod för att kunna kör den rekursivt, passerar in requesten och html referensen för att kunna använda samma metod för flera saker
+  var successElement = document.getElementById("test"); //tar ett element ur htmlen
+  var errorElement = document.getElementById("operationResult")
+  successElement.innerHTML =  "Adding book..."; //skriver bara 'adding book'
+  fetchFromApi(fetchRequest, successElement, errorElement, counter); //kallar på en metod för att kunna kör den rekursivt, passerar in requesten och html referensen för att kunna använda samma metod för flera saker
 }
 
-function fetchFromApi(fetchRequest, htmlElement, counter){
+function fetchFromApi(fetchRequest, successElement, errorElement, counter){
   fetch(fetchRequest) //fetchar requesten
   .then((response) => 
     response.json()) //gör om till json
@@ -27,12 +28,13 @@ function fetchFromApi(fetchRequest, htmlElement, counter){
     console.log(jsonResponse.status);  //loggar statusen i debug syfte
     if(jsonResponse.status == "error"){  //om statusen är 'error' 
       if(counter >= 10){ 
-        htmlElement.innerHTML = jsonResponse.message; //self explanatory
+        errorElement.innerHTML = jsonResponse.message; //self explanatory
+        successElement.innerHTML = "";
       } else {
-        fetchFromApi(fetchRequest, htmlElement, counter + 1);  //kör samma metod igen
+        fetchFromApi(fetchRequest, successElement, errorElement, counter + 1);  //kör samma metod igen
       }
     } else {
-    htmlElement.innerHTML = jsonResponse.status; //skriv ut status på 'transaktionen'
+    successElement.innerHTML = jsonResponse.status; //skriv ut status på 'transaktionen'
     }
   });
 }
